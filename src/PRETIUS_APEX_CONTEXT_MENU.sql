b9d010841870eb99c9119397af7ed4fb867873b6
akierzkowski <akierzkowski@gmail.com>
2021-01-26 21:25:28 +0100

    Fixing difference in source package name between package body and spec Change in name of plugin install file

--- src/PRETIUS_APEX_CONTEXT_MENU.sql
+++ src/PRETIUS_APEX_CONTEXT_MENU.sql
@@ -1,8 +1,8 @@
-create or replace package PRETIUS_APEX_CONTEXT_MENU_V110 as
+create or replace package PRETIUS_APEX_CONTEXT_MENU as
 
   function render (
     p_dynamic_action in apex_plugin.t_dynamic_action,
     p_plugin         in apex_plugin.t_plugin 
   ) return apex_plugin.t_dynamic_action_render_result;
 
-end PRETIUS_APEX_CONTEXT_MENU_V110;
+end PRETIUS_APEX_CONTEXT_MENU;
